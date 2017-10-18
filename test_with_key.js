4e3074cf746edcd537b4fa9fa4b45d5c787872a5
Pasqualino de Simone <pasalino@gmail.com>
2017-10-18 16:40:44 +0200

    keys

--- test_with_key.js
+++ test_with_key.js
@@ -1,26 +1,39 @@
+
 var fs = require('fs');
 var NodeRSA = require('node-rsa');
 
 
+var private_a = fs.readFileSync('./keys/privkey_a.pem', 'utf8');
+var public_a = fs.readFileSync('./keys/pubkey_a.pem', 'utf8');
+
+var private_b = fs.readFileSync('./keys/privkey_b.pem', 'utf8');
+var public_b = fs.readFileSync('./keys/pubkey_b.pem', 'utf8');
+
 console.log('=========================');
 
-var keya = new NodeRSA({b:1024});
-var keyb = new NodeRSA({b:1024});
+var keya = new NodeRSA(private_a,'pkcs1');
+var keyb = new NodeRSA(private_b,'pkcs1');
+var keya_p = new NodeRSA();
+keya_p.importKey(public_a, 'pkcs8-public-pem');
+var keyb_p = new NodeRSA();
+keyb_p.importKey(public_b, 'pkcs8-public-pem');
+
+
 
 console.log('=========================');
 var text = 'Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{* Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{* Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{* Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{* Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{*Un testo molto lungo cerchiamo di capire cosa succede. Lorem ipsum ecc.. ec...c d3465753403{}{}{*';
-var encrypted = keya.encrypt(text, 'base64');
+var encrypted = keya.encryptPrivate(text, 'base64');
 console.log('encrypted a: ', encrypted);
 console.log('=========================');
-var encrypted = keyb.encrypt(encrypted, 'base64');
+var encrypted = keyb.encryptPrivate(encrypted, 'base64');
 console.log('encrypted b: ', encrypted);
 console.log('=========================');
 
 
-var decrypted = keyb.decrypt(encrypted, 'utf8');
+var decrypted = keyb_p.decryptPublic(encrypted, 'utf8');
 console.log('decrypted b: ', decrypted);
 console.log('=========================');
-var decrypted = keya.decrypt(decrypted, 'utf8');
+var decrypted = keya_p.decryptPublic(decrypted, 'utf8');
 console.log('decrypted a: ', decrypted);
 console.log('=========================');
 
